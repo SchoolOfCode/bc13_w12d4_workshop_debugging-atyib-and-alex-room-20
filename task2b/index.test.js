6dab0e537128333344112008ce3bf4ad84c3f9bd
Muhammed Atyib Ali <aatyib@gmail.com>
2022-12-15 17:02:30 +0000

    task 2b done

--- task2b/index.test.js
+++ task2b/index.test.js
@@ -14,23 +14,23 @@ test("The hasPets function should return (henry ford really does have a nice pet
   );
 });
 
-test("The hasPets function should return (henry ford has no pets)", () => {
-  expect(calcs.hasPets(false, "henry", "ford")).toBe("henry ford has no pets");
-});
+ test("The hasPets function should return (henry ford has no pets)", () => {
+   expect(calcs.hasPets(false, "henry", "ford")).toBe("henry ford has no pets");
+ });
 
-test("The balance of account1 and account2 should return 156", () => {
-  expect(calcs.addsBalanceOfAccounts()).toBe(156);
-});
+ test("The balance of account1 and account2 should return 156", () => {
+   expect(calcs.addsBalanceOfAccounts()).toBe(156);
+ });
 
-test("The Calculator should return 4", () => {
-  expect(calcs.calculator(2, "+", 2)).toBe(4);
+ test("The Calculator should return 4", () => {
+   expect(calcs.calculator(2, "+", 2)).toBe(4);
 });
-test("The Calculator should return 2", () => {
-  expect(calcs.calculator(5, "-", 3)).toBe(2);
+ test("The Calculator should return 2", () => {
+   expect(calcs.calculator(5, "-", 3)).toBe(2);
 });
-test("The Calculator should return 12", () => {
-  expect(calcs.calculator(3, "*", 4)).toBe(12);
-});
+ test("The Calculator should return 12", () => {
+   expect(calcs.calculator(3, "*", 4)).toBe(12);
+ });
 test("The Calculator should return 5", () => {
-  expect(calcs.calculator(15,"/",3)).toBe(5);
-});
+   expect(calcs.calculator(15,"/",3)).toBe(5);
+ });
